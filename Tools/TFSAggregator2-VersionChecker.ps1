8bdef16b58025c2105e56ab12de8d25470059df7
M-Luke <15806408+M-Luke@users.noreply.github.com>
2023-11-04 23:09:23 +0000

    Script cleanup

--- Tools/TFSAggregator2-VersionChecker.ps1
+++ Tools/TFSAggregator2-VersionChecker.ps1
@@ -1,15 +1,26 @@
 ﻿#Script will error if TFS/DevOps is not installed on the machine you are running it on! (AKA "HKLM:\SOFTWARE\Microsoft\TeamFoundationServer" is missing in registry)
+$baseTFSRegistryPath = "HKLM:\SOFTWARE\Microsoft\TeamFoundationServer"
+
+if (!(Test-Path $baseTFSRegistryPath)) {
+	throw "TFS/DevOps server does not appear to be installed? (Not found in registry!)"
+}
 
 $highestTFSversion = "{0:N1}" -f (
-	Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\TeamFoundationServer" |
+	Get-ChildItem -Path $baseTFSRegistryPath |
 		Split-Path -Leaf |
 		Foreach-Object { $_ -as [double] } |
 		Sort-Object -Descending |
 		Select-Object -First 1)
 
-$tfsRegistryInfo = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\TeamFoundationServer\$highestTFSversion"
+$tfsRegistryInfo = Get-ItemProperty -Path "$baseTFSRegistryPath\$highestTFSversion"
 
-Write-Output "Installed TFS/DevOps Server BuildNumber: $($tfsRegistryInfo.BuildNumber)`n`n"
+
+if($tfsRegistryInfo.BuildNumber -gt $tfsRegistryInfo.PatchVersion){
+	Write-Output "Installed TFS/DevOps Server - BuildNumber: $($tfsRegistryInfo.BuildNumber)`n`n"
+}
+else{
+	Write-Output "Installed TFS/DevOps Server - BuildNumber: $($tfsRegistryInfo.BuildNumber) | PatchVersion: $($tfsRegistryInfo.PatchVersion)`n`n"
+}
 
 
 $pathToAssemblyFile = "$($tfsRegistryInfo.InstallPath)Application Tier\Web Services\bin\Plugins\TFSAggregator2.ServerPlugin.dll"
